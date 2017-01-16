6fc074b7e6bccfd44375e8956adcbeadf1031c03
beusj <BeusJ@email.chop.edu>
2017-01-15 22:45:05 -0500

    return caret at end box

--- js/bp_autocomplete.js
+++ js/bp_autocomplete.js
@@ -1,6 +1,6 @@
 
   var BP_SEARCH_SERVER = "http://data.bioontology.org";
-  var LIST_ONTOLOGY = "SNOMEDCT"
+  var LIST_ONTOLOGY = "SNOMEDCT";
   
   $( function() {
 
@@ -10,13 +10,13 @@
       // data = data.responseJSON.collection
       for (var i=0; i < data.length; i++) {
         result = {};
-        var line = data[i]
-        result['id'] = line.prefLabel;
-        result['value'] = line.prefLabel;
-        result['uri'] = line["@id"];
-        result['cui'] = (line.cui) ? line.cui[0]:"";
-        result['synonym'] = (line.synonym) ? line.synonym[0]:""; //if not an avaiolable property, leave blank
-        parsed.push(result)
+        var line = data[i];
+        result.id = line.prefLabel;
+        result.value = line.prefLabel;
+        result.uri = line["@id"];
+        result.cui = (line.cui) ? line.cui[0]:"";
+        result.synonym = (line.synonym) ? line.synonym[0]:""; //if not an avaiolable property, leave blank
+        parsed.push(result);
       }
       return parsed;
     }
@@ -35,14 +35,28 @@
       return split( term ).splice(0,-1);
     }
 
-
-
-
-    //Still need to figure out how to append new results
+// return caret to end of line
+// http://stackoverflow.com/questions/4233265/contenteditable-set-caret-at-the-end-of-the-text-cross-browser
+    function placeCaretAtEnd(el) {
+        el.focus();
+        if (typeof window.getSelection != "undefined"
+                && typeof document.createRange != "undefined") {
+            var range = document.createRange();
+            range.selectNodeContents(el);
+            range.collapse(false);
+            var sel = window.getSelection();
+            sel.removeAllRanges();
+            sel.addRange(range);
+        } else if (typeof document.body.createTextRange != "undefined") {
+            var textRange = document.body.createTextRange();
+            textRange.moveToElementText(el);
+            textRange.collapse(false);
+            textRange.select();
+        }
+    }
 
     function formatSelect ( event, ui ) {
-      // var terms = split( this );
-      var prev = split($(this).html()).slice(0,-1)
+        var prev = split($(this).html()).slice(0,-1);
 
         var spanForm = "<span contenteditable=false style='color:blue' id=" +
           encodeURI(ui.item.id) +
@@ -56,18 +70,16 @@
           ui.item.value + 
           "</span>" +
           ("; ") ;
-        // if (spans.length == 0) {
-      prev.push(spanForm);
+        
+        prev.push(spanForm);
 
-          $(this).html(prev.join("; "));
-        // } else {
-          // $(this).html(me[0].outerHTML + spanForm + "; ")
-          // $(this).append(spanForm + "; ")
-          // spans.append(spanForm + "; ")
-        // }
-        console.log($("#ac").html())
+        $(this).html(prev.join("; "));
+        // console.log($("#ac").html());
+        // console.log($(this).text().length);
+        placeCaretAtEnd($(this).get(0));
         return false;
       }
+
 
     $( "#ac" )
       .on( "keydown", function( event ) {
@@ -85,25 +97,23 @@
             encodeURI(LIST_ONTOLOGY) +
             "&suggest=true" +
             "&format=jsonp" +
-            "&callback=?"
-            // apikey: encodeURI("879f0065-476f-4769-8765-3d8a44a6dde1")
-          ,
+            "&callback=?",
           function(data) {
               // console.log(data);
               data = parseData(data.collection);
               // console.log(data);
               response(data);
             }
-          )
+          );
         },
-        minLength: 3
-        ,
+        minLength: 3,
         focus: function() {
             // prevent value inserted on focus
+            // var strLength = $(this).val().length;
+            // $("ac").setSelectionRange(strLength,strLength)
             return false;
-        }
-        ,
+        },
         select: formatSelect
       }
     );
-  })
+  });
