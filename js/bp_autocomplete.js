91ea6d6ffc8c145d79c94caf939141ae3b3077fe
beusj <BeusJ@email.chop.edu>
2017-01-27 23:38:17 -0500

    allow insertion of autocomplete in middle of text

--- js/bp_autocomplete.js
+++ js/bp_autocomplete.js
@@ -3,11 +3,12 @@
 // - generalize to other content types
 var BP_SEARCH_SERVER = "https://data.bioontology.org";
 var LIST_ONTOLOGY = "ICD10";
+window.rememberText = "";
 //API Reference http://data.bioontology.org/documentation#Class
 
 $(function() {
 
-    function parseData(data) {
+    function parseData(data, term) {
         if (!data) return null;
         var parsed = [];
         // data = data.responseJSON.collection
@@ -22,6 +23,7 @@ $(function() {
             result.ontcode = (ontcode) ? ontcode : ''; //if exists, make it that; else if null than make a blank string
             result.cui = (line.cui) ? line.cui[0] : ''; //concept unique identifier specific to bioportal
             result.synonym = (line.synonym) ? line.synonym[0] : ''; //if not an available property, leave blank
+            result.searchTerm = term;
             parsed.push(result); //add results from most recent iteration to parsed array
         }
         return parsed;
@@ -42,21 +44,24 @@ $(function() {
     }
 
     //http://stackoverflow.com/questions/16736680/get-caret-position-in-contenteditable-div-including-tags
-    function getCaretCharacterOffsetWithin(element) {
+    function getCaretCharacterOffsetWithin(el) {
+        // var el = el.element[0];
+        // var el = document.getElementById("ac");
+        // console.log(el);
         var caretOffset = 0;
-    // if (typeof window.getSelection != "undefined") {
-    //     var range = window.getSelection().getRangeAt(0);
-    //     var preCaretRange = range.cloneRange();
-    //     preCaretRange.selectNodeContents(element);
-    //     preCaretRange.setEnd(range.endContainer, range.endOffset);
-    //     caretOffset = preCaretRange.toString().length;
-    // } else if (typeof document.selection != "undefined" && document.selection.type != "Control") {
+    if (typeof window.getSelection != "undefined") {
+        var range = window.getSelection().getRangeAt(0);
+        var preCaretRange = range.cloneRange();
+        preCaretRange.selectNodeContents(el);
+        preCaretRange.setEnd(range.endContainer, range.endOffset);
+        caretOffset = preCaretRange.toString().length;
+    } else if (typeof document.selection != "undefined" && document.selection.type != "Control") {
         var textRange = document.selection.createRange();
         var preCaretTextRange = document.body.createTextRange();
-        preCaretTextRange.moveToElementText(element);
+        preCaretTextRange.moveToElementText(el);
         preCaretTextRange.setEndPoint("EndToEnd", textRange);
-        // caretOffset = preCaretTextRange.text.length;
-    // }
+        caretOffset = preCaretTextRange.text.length;
+    }
     return caretOffset;
     };
 
@@ -81,7 +86,21 @@ $(function() {
     }
 
     function formatSelect(event, ui) {
-        var prev = split_semi($(this).html()).slice(0, -1);
+        console.log($(this));
+        // var caretPos = getCaretCharacterOffsetWithin(this);
+        var htmlStr = $(this).get(0).innerHTML;
+        var searchTerm = ui.item.searchTerm
+        // var htmlArray = htmlStr.split(ui.item.searchTerm);
+        // console.log(htmlArray);
+        // console.log(htmlArray);
+        // var before = htmlArray[0];
+        var before = htmlStr.substring(0,htmlStr.indexOf(searchTerm));
+        before  = (before) ? before : "";
+        // var after = htmlArray[-1];
+        var after = htmlStr.substring(htmlStr.indexOf(searchTerm) + searchTerm.length);
+        after = (after) ? after : "";
+        // console.log(after.join(""))
+
 
         // create programatically as jquery object?
         // var spanForm = $("<span />").addClass("diagnosis"
@@ -109,12 +128,10 @@ $(function() {
             // " data-synonym=" +
             // encodeURI(ui.item.synonym) +
             ">" +
-            ui.item.value + //consider putting in user entered text, using function extractLast()
-            "</span>" 
-            // + ("; ")
-            ;
-
-        prev.push(spanForm);
+            ui.item.value + 
+            // ui.item.searchTerm + //consider putting in user entered text, using function extractLast()
+            "</span>" ;
+            // + ("; ");
 
         //mouse over action
         $("body").on('mouseenter', "div.ui-widget #ac span.diagnosis",
@@ -138,11 +155,17 @@ $(function() {
                 return false;
             }
         );
-
-        $(this).html(prev.join("; "));
+        // lastContent = before + spanForm  + after;
+        $(this).html(before + spanForm  + after);
+        window.rememberText = this.text;
+        console.log(before,spanForm,after);
         placeCaretAtEnd($(this).get(0));
         return false;
     }
+
+    $("ac").change( function() {
+            log.console("div was changed");
+        });
 
     $("#ac")
         .on("keydown", function(event) {
@@ -151,10 +174,36 @@ $(function() {
                 event.preventDefault();
             }
         })
-        .autocomplete({
+        .autocomplete(
+            {
             source: function(request, response) {
-                var caretPos = getCaretCharacterOffsetWithin(this)
+                var caretPos = getCaretCharacterOffsetWithin(this.element[0]);
+                // var strRemove = window.rememberText.split("");
+                // console.log(window.rememberText);
+                // var before = window.rememberText.substring(0,caretPos).trim();
+                // var after = window.rememberText.substring(caretPos).trim();
+                // console.log(caretPos,before,after);
+                // term = request.term;
+                // before_pos = term.indexOf(before);
+                // after_pos = term.indexOf(after);
+                // if (before.length === 0) {before_pos = -1;}
+                // if (after.length === 0) {after_pos = -1;}
+                // if (before_pos > -1 && before.length > 0 && after_pos > -1 && after.length > 0) {
+                //     searchTerm = term.substring(before_pos + before.length, Math.min(after_pos,term.length));
+                // } else if (before_pos > -1 && before.length > 0 && after_pos < 0) {
+                //     searchTerm = term.substring(before_pos + before.length);
+                // } else if (before_pos < 0 && after_pos > -1 && after.length > 0) {
+                //     searchTerm = term.substring(0, Math.min(after_pos,term.length));
+                // } else {
+                //     searchTerm  = term;
+                // };
+                // console.log("substring: " + searchTerm, caretPos, "before: " + before, "b_pos: " + before_pos, "after: " + after, "a_pos: " + after_pos);
                 var searchTerm = request.term.substring(0,caretPos).split(/\b/).pop();
+                // var contentStr = $(this)["0"].element["0"].innerText;
+                // console.log(contentStr);
+                // var htmlStr = $(this)["0"].element["0"].innerHTML;
+                // var htmlArray = htmlStr.split(searchTerm);
+                // console.log(htmlArray);
                 $.getJSON(BP_SEARCH_SERVER +
                     "/search?q=" +
                     // encodeURI(request.term.split(/\b/).pop()) +
@@ -167,24 +216,24 @@ $(function() {
                     "&callback=?",
                     function(data) {
                         console.log(data);
-                        data = parseData(data.collection);
+                        data = parseData(data.collection,searchTerm);
                         // console.log(data);
                         response(data);
                     }
                 );
-                console.log("substr: " + request.term.substring(0,caretPos));
-                console.log("search term: " + searchTerm);
-                console.log(caretPos);
+                // console.log("substr: " + request.term.substring(0,caretPos));
+                // console.log("search term: " + searchTerm);
+                // console.log(caretPos);
                 // console.log(this.element);
             },
             minLength: 3,
             focus: function() {
                 return false;
             },
-            // change: function(event,ui) {
-
-            // }
-            // ,
+            change: function(event,ui) {
+                window.rememberText = $(this).text();
+            }
+            ,
             select: formatSelect
         });
 });
